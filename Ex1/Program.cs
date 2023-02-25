5c521737ae18ef672e673fad00c2ac37bafdcf18
NorthMan38IDE <dr.laitman@mail.ru>
2023-02-25 13:35:07 +0700

    Ex2 Completed

---
+++ Ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GenArray(int length){ // Генератор
+    int[] mass = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = rnd.Next(1, 1000);
+    }
+    return mass;}
+//----------------------------------------------
+void PrintArr(int[] array){                // Вывод массива
+    for (int i = 0; i < array.Length; i++)
+    {   
+        System.Console.Write(array[i] + " ");
+    }}
+//---------------------------------------------
+int CountElements(int[] array){            // Счет четных элементов
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            count = count + 1;
+        }
+        
+    }
+    return count;}
+//---------------------------------------------
+int InputSize(string msg){                  // Ввод размера массива
+     System.Console.Write($"{msg}");
+     return Convert.ToInt32(System.Console.ReadLine());}
+//---------------------------------------------
+
+int sizeArray = InputSize("введите размер массива > ");
+int[] array = GenArray(sizeArray);
+System.Console.WriteLine("--------------------------");
+System.Console.Write("Массив элементов:");
+PrintArr(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество чётных чисел в массиве: {CountElements(array)}");
+
---
+++ Ex2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] GenArray(int length){                // Генератор
+    int[] mass = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = rnd.Next(1, 100);
+    }
+    return mass;}
+//----------------------------------------------
+void PrintArr(int[] array){                // Вывод массива
+    for (int i = 0; i < array.Length; i++)
+    {   
+        System.Console.Write(array[i] + " ");
+    }}
+//---------------------------------------------
+int SumPositiveElements(int[] array){      // Сумма элементов, находящихся на нечетных индексах элементов
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+        
+    }
+    return sum;}
+//---------------------------------------------
+int InputSize(string msg){                 // Ввод размера массива
+     System.Console.Write($"{msg}");
+     return Convert.ToInt32(System.Console.ReadLine());}
+//---------------------------------------------
+
+int sizeArray = InputSize("введите размер массива > ");
+int[] array = GenArray(sizeArray);
+System.Console.WriteLine("--------------------------");
+System.Console.Write("Массив элементов:");
+PrintArr(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Сумма элементов, находящихся на нечетных индексах в массиве равна: {SumPositiveElements(array)}");
